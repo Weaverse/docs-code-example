271087d277be927dd2d8e447fee69c3e00bd23b7
Tuan Anh (Leo) Huynh <huynhtuananh21895@gmail.com>
2023-09-10 12:03:00 +0700

    Update schema.ts

--- theme-overview/schema.ts
+++ theme-overview/schema.ts
@@ -1,54 +1,31 @@
 export let schema: HydrogenComponentSchema = {
-  type: 'hero',
-  title: 'Hero',
+  type: 'featured-products',
+  title: 'Featured products',
+  limit: 1,
+  enabledOn: {
+    pages: ['INDEX'],
+  },
   inspector: [
     {
-      group: 'Hero',
+      group: 'Featured products',
       inputs: [
         {
-          type: 'collection',
-          name: 'collectionHandle',
-          label: 'Preview',
-          defaultValue: 'frontpage',
+          type: 'text',
+          name: 'heading',
+          label: 'Heading',
+          defaultValue: 'Featured Products',
+          placeholder: 'Featured Products',
         },
         {
-          type: 'toggle-group',
-          name: 'loading',
-          label: 'Background image loading',
-          defaultValue: 'eager',
+          type: 'range',
+          name: 'productsCount',
+          label: 'Number of products',
+          defaultValue: 12,
           configs: {
-            options: [
-              {label: 'Eager', value: 'eager', icon: 'Lightning'},
-              {
-                label: 'Lazy',
-                value: 'lazy',
-                icon: 'SpinnerGap',
-                weight: 'light',
-              },
-            ],
+            min: 1,
+            max: 12,
+            step: 1,
           },
-          helpText:
-            'Learn more about <a href="https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement/loading" target="_blank" rel="noopener noreferrer">image loading strategies</a>.',
-        },
-        {
-          type: 'select',
-          name: 'height',
-          label: 'Height',
-          configs: {
-            options: [
-              {label: 'Auto', value: 'auto'},
-              {label: 'Fullscreen', value: 'full'},
-            ],
-          },
-          defaultValue: 'auto',
-        },
-        {
-          type: 'switch',
-          name: 'top',
-          label: 'Top',
-          defaultValue: true,
-          helpText:
-            'Push the hero to the top of the page by adding a negative margin.',
         },
       ],
     },
