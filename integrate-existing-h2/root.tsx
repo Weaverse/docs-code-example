c840ca9e76eff5f86ee2320f32dc264a2cc40db1
Minh Phan (Paul) <phanminh65@gmail.com>
2023-09-25 12:37:16 +0700

    Create root.tsx

---
+++ integrate-existing-h2/root.tsx
@@ -0,0 +1,23 @@
+// app/root.tsx
+export async function loader({request, context}: RouteLoaderArgs) {
+  const {session, storefront, cart} = context;
+  const [customerAccessToken, layout] = await Promise.all([
+    session.get('customerAccessToken'),
+    getLayoutData(context),
+  ]);
+
+  const seo = seoPayload.root({shop: layout.shop, url: request.url});
+
+  return defer({
+    isLoggedIn: Boolean(customerAccessToken),
+    layout,
+    selectedLocale: storefront.i18n,
+    cart: cart.get(),
+    analytics: {
+      shopifySalesChannel: ShopifySalesChannel.hydrogen,
+      shopId: layout.shop.id,
+    },
+    seo,
+    weaverseTheme: await context.weaverse.loadThemeSettings(),
+  });
+}
