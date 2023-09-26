a140dfbe036f02b7c1d06054e479f00d8fc14672
Minh Phan (Paul) <phanminh65@gmail.com>
2023-09-26 11:10:07 +0700

    Create weaverse-component-02.ts

---
+++ integrate-existing-h2/weaverse-component-02.ts
@@ -0,0 +1,8 @@
+import type {HydrogenComponent} from '@weaverse/hydrogen';
+import * as Main from '~/sections/main';
+import * as Video from '~/sections/video';
+
+export let components: HydrogenComponent[] = [
+  Main,
+  Video
+];
