4fd20c57c4e072c65a25482e2334e796c197e726
Tuan Anh (Leo) Huynh <huynhtuananh21895@gmail.com>
2023-10-08 21:51:22 +0700

    Update featured-products.tsx

--- theme-section-overview/featured-products.tsx
+++ theme-section-overview/featured-products.tsx
@@ -1,3 +1,9 @@
+interface FeaturedProductsProps
+  extends HydrogenComponentProps<Awaited<ReturnType<typeof loader>>> {
+  heading: string;
+  productsCount: number;
+}
+
 let FeaturedProducts = forwardRef<HTMLElement, FeaturedProductsProps>(
   (props, ref) => {
     let {loaderData, heading, productsCount, ...rest} = props;
