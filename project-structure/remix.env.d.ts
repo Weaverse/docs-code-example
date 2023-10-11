14d925355283872e9d9361c7845e7cc1a3af3f04
Tuan Anh (Leo) Huynh <huynhtuananh21895@gmail.com>
2023-10-11 15:40:08 +0700

    Create remix.env.d.ts

---
+++ project-structure/remix.env.d.ts
@@ -0,0 +1,30 @@
+/**
+ * Declare expected Env parameter in fetch handler.
+ */
+interface Env {
+  SESSION_SECRET: string;
+  PUBLIC_STOREFRONT_API_TOKEN: string;
+  PRIVATE_STOREFRONT_API_TOKEN: string;
+  PUBLIC_STORE_DOMAIN: string;
+  PUBLIC_STOREFRONT_ID: string;
+  /**
+   * Include the Weaverse Project's Id - you'll find this in the Weaverse Editor
+   * under Project Settings.
+   * And the optional Weaverse Host - which value is https://weaverse.io by default.
+   */
+  WEAVERSE_PROJECT_ID: string;
+  WEAVERSE_HOST: string;
+}
+
+/**
+ * Declare local additions to the Remix loader context.
+ */
+export interface AppLoadContext {
+  waitUntil: ExecutionContext['waitUntil'];
+  session: HydrogenSession;
+  storefront: Storefront;
+  cart: HydrogenCart;
+  env: Env;
+  // Include the Weaverse Client in the Remix loader context.
+  weaverse: WeaverseClient;
+}
