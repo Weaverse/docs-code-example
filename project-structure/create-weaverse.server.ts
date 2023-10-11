69e1b91a3ac578e626a8f47aab8b62b23ca99348
Tuan Anh (Leo) Huynh <huynhtuananh21895@gmail.com>
2023-10-11 17:01:19 +0700

    Update create-weaverse.server.ts

--- project-structure/create-weaverse.server.ts
+++ project-structure/create-weaverse.server.ts
@@ -1,3 +1,9 @@
+import {Storefront} from '@shopify/hydrogen';
+import {I18nLocale, WeaverseClient} from '@weaverse/hydrogen';
+import {countries} from '~/data/countries';
+import {components} from '~/weaverse/components';
+import {themeSchema} from '~/weaverse/schema.server';
+
 type CreateWeaverseArgs = {
   storefront: Storefront<I18nLocale>;
   request: Request;
