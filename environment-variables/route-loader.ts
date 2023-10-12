fcbf6760bec3bf2c163efa3f0f1af433726306aa
Tuan Anh (Leo) Huynh <huynhtuananh21895@gmail.com>
2023-10-12 10:43:39 +0700

    Update route-loader.ts

--- environment-variables/route-loader.ts
+++ environment-variables/route-loader.ts
@@ -1,3 +1,5 @@
+import {type LoaderArgs, json} from '@shopify/remix-oxygen';
+
 export async function loader({context}: LoaderArgs) {
   // Get `env` variables from App Load Context
   let {env} = context
