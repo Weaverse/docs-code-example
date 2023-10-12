0f64818bc3635727db02a3fcbf7b0e2dd56a06b0
Tuan Anh (Leo) Huynh <huynhtuananh21895@gmail.com>
2023-10-12 15:59:45 +0700

    Create example-schema.ts

---
+++ component-schema/example-schema.ts
@@ -0,0 +1,21 @@
+import type { HydrogenComponentSchema } from '@weaverse/hydrogen';
+
+let exampleSchema: HydrogenComponentSchema = {
+  title: 'Product Card',
+  type: 'product-card',
+  childTypes: ['image', 'product-title', 'price'],
+  presets: {
+    type: 'product-card',
+    children: [
+      { type: 'image' },
+      { type: 'product-title' },
+      { type: 'price' },
+    ],
+  },
+  limit: 3,
+  enabledOn: {
+    pages: ['INDEX', 'PRODUCT', 'ALL_PRODUCTS'],
+    groups: ['body'],
+  },
+  toolbar: ['general-settings', ['duplicate', 'delete']],
+};
