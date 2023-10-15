a10383c465bc0f82051df8231659377dc3700af8
Tuan Anh (Leo) Huynh <huynhtuananh21895@gmail.com>
2023-10-15 09:08:43 +0700

    Create textarea.ts

---
+++ input-settings/textarea.ts
@@ -0,0 +1,7 @@
+{
+  type: "textarea",
+  label: "Customer Testimonial",
+  name: "customerTestimonial",
+  defaultValue: "The shipping was fast, and the packaging was eco-friendly. I love shopping here!",
+  placeholder: "Share customer shopping experience..."
+}
