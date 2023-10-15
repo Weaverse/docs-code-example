388802bf1c1814d64f06788724e58e6d3b89867f
Tuan Anh (Leo) Huynh <huynhtuananh21895@gmail.com>
2023-10-15 10:07:59 +0700

    Create select.ts

---
+++ input-settings/select.ts
@@ -0,0 +1,14 @@
+{
+  type: "select",
+  label: "Image aspect ratio",
+  name: "imageAspectRatio",
+  configs: {
+    options: [
+      { value: "auto", label: "Adapt to image" },
+      { value: "1/1", label: "1/1" },
+      { value: "3/4", label: "3/4" },
+      { value: "4/3", label: "4/3" },
+    ]
+  },
+  defaultValue: "auto"
+}
