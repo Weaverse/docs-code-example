67d561df8f638752ca80f5ee55011ee590eeb8fc
Tuan Anh (Leo) Huynh <huynhtuananh21895@gmail.com>
2023-10-15 11:32:22 +0700

    Create toggle-group-as-text.ts

---
+++ input-settings/toggle-group-as-text.ts
@@ -0,0 +1,13 @@
+{
+  type: 'toggle-group',
+  label: 'Gallery size',
+  name: 'gallerySize',
+  configs: {
+    options: [
+      {value: 'small', label: 'Small'},
+      {value: 'medium', label: 'Medium'},
+      {value: 'large', label: 'Large'},
+    ],
+  },
+  defaultValue: 'large',
+},
