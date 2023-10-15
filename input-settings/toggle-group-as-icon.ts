f813cceee7592df7b37eb884bf3d7ce705acf6b9
Tuan Anh (Leo) Huynh <huynhtuananh21895@gmail.com>
2023-10-15 11:38:07 +0700

    Create toggle-group-as-icon.ts

---
+++ input-settings/toggle-group-as-icon.ts
@@ -0,0 +1,12 @@
+{
+  type: "toggle-group",
+  name: "loading",
+  label: "Background image loading",
+  configs: {
+    options: [
+      {label: "Eager", value: "eager", icon: "Lightning"},
+      {label: "Lazy", value: "lazy", icon: "SpinnerGap"},
+    ],
+  },
+  defaultValue: "eager",
+}
