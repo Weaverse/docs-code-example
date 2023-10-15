a66f5b469e999f521801be6015721d7828b49bf1
Tuan Anh (Leo) Huynh <huynhtuananh21895@gmail.com>
2023-10-15 09:14:02 +0700

    Create switch.ts

---
+++ input-settings/switch.ts
@@ -0,0 +1,7 @@
+{
+  type: "switch",
+  label: "Enable Promotional Discount",
+  name: "enableDiscount",
+  defaultValue: true,
+  helpText: "Toggle to enable or disable the promotional discount for this product."
+}
