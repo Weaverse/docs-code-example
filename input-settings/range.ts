6b5ffc922e27f5dff5818c6aa13a313f631cdd45
Tuan Anh (Leo) Huynh <huynhtuananh21895@gmail.com>
2023-10-15 09:21:51 +0700

    Create range.ts

---
+++ input-settings/range.ts
@@ -0,0 +1,12 @@
+{
+  type: "range",
+  label: "Discount percentage",
+  name: "discountPercentage",
+  defaultValue: 10,
+  configs: {
+    min: 5,
+    max: 50,
+    step: 1,
+    unit: "%"
+  }
+}
