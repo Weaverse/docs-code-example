4a7603690a8a731a10e20a09c0d2f2323a1feb2c
Tuan Anh (Leo) Huynh <huynhtuananh21895@gmail.com>
2023-10-16 10:37:35 +0700

    Create richtext.ts

---
+++ input-settings/richtext.ts
@@ -0,0 +1,6 @@
+{
+  "type": "richtext",
+  "label": "Promotion details",
+  "name": "promotionDetails",
+  "defaultValue": "<p>We're excited to announce our <strong>limited-time</strong> savings event. From <em>October 15th to November 15th</em>, enjoy exclusive discounts and offers.</p>"
+}
