2df1f2f948a63ab94379bdc0b9ebff949b5e8742
Tuan Anh (Leo) Huynh <huynhtuananh21895@gmail.com>
2023-10-16 11:20:48 +0700

    Create image.ts

---
+++ input-settings/image.ts
@@ -0,0 +1,6 @@
+{
+  type: 'image',
+  name: 'authorImage',
+  label: 'Author image',
+  defaultValue: 'https://cdn.shopify.com/s/files/1/0728/0410/6547/files/wv-fashion-model-in-fur.jpg?v=1694236467',
+}
