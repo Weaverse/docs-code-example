eb4887f3bf8a37b65cb97a0bc2dd64f05c687bc6
Tuan Anh (Leo) Huynh <huynhtuananh21895@gmail.com>
2023-10-16 16:01:54 +0700

    Update image.ts

--- input-settings/image.ts
+++ input-settings/image.ts
@@ -8,4 +8,9 @@
     width: 689,
     height: 1034,
   },
+  // The following are acceptable as well:
+  // defaultValue: {
+  //   url: "https://cdn.shopify.com/s/files/1/0669/0262/2504/files/linkedin-sales-solutions-pAtA8xe_iVM-unsplash.jpg?v=1697429747",
+  // },
+  // defaultValue: "https://cdn.shopify.com/s/files/1/0669/0262/2504/files/linkedin-sales-solutions-pAtA8xe_iVM-unsplash.jpg?v=1697429747",
 }
