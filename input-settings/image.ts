104eb0ced87a025c380720125cc72d4649f5fb51
Tuan Anh (Leo) Huynh <huynhtuananh21895@gmail.com>
2023-10-17 10:20:56 +0700

    Update image.ts

--- input-settings/image.ts
+++ input-settings/image.ts
@@ -1,7 +1,7 @@
 {
-  type: 'image',
-  name: 'authorImage',
-  label: 'Author image',
+  type: "image",
+  name: "authorImage",
+  label: "Author image",
   defaultValue: {
     url: "https://cdn.shopify.com/s/files/1/0669/0262/2504/files/linkedin-sales-solutions-pAtA8xe_iVM-unsplash.jpg?v=1697429747",
     alt: "Man standing beside wall",
