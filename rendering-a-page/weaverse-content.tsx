316958df8ab3f95d2808bdb78d6417a72f725592
Tuan Anh (Leo) Huynh <huynhtuananh21895@gmail.com>
2023-10-23 21:48:32 +0700

    Update weaverse-content.tsx

--- rendering-a-page/weaverse-content.tsx
+++ rendering-a-page/weaverse-content.tsx
@@ -1,3 +1,5 @@
+// <root>/app/weaverse/index.tsx
+
 import {WeaverseHydrogenRoot} from '@weaverse/hydrogen';
 import {GenericError} from '~/components/GenericError';
 import {components} from './components';
@@ -5,9 +7,7 @@ import {components} from './components';
 export function WeaverseContent() {
   return (
     <WeaverseHydrogenRoot
-      // All the components that are used in the Weaverse page
       components={components}
-      // Optional error component to render if any error occurs
       errorComponent={GenericError}
     />
   );
