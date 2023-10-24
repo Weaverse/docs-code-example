b315cf5c8b564c67f5073897066087bc104595c7
Tuan Anh (Leo) Huynh <huynhtuananh21895@gmail.com>
2023-10-24 18:49:10 +0700

    Update fetch-w-cache-long.ts

--- data-fetching-and-caching/fetch-w-cache-long.ts
+++ data-fetching-and-caching/fetch-w-cache-long.ts
@@ -2,7 +2,7 @@ import type {ComponentLoaderArgs} from '@weaverse/hydrogen';
 
 export let loader = async ({weaverse}: ComponentLoaderArgs) => {
   let {fetchWithCache, storefront} = weaverse;
-  return await fetchWithCache(`https://external-api`, {
+  return await fetchWithCache(`https://external-api.endpoint`, {
     strategy: storefront.CacheLong(),
     // request init options...
   });
