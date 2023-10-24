736be4c1c9f096b91d74d69f1aea372e173b3d84
Tuan Anh (Leo) Huynh <huynhtuananh21895@gmail.com>
2023-10-24 16:02:44 +0700

    Update product-query.ts

--- data-fetching-and-caching/product-query.ts
+++ data-fetching-and-caching/product-query.ts
@@ -14,15 +14,17 @@ type MyComponentData = {
 // Component definition...
 
 export let loader = async (args: ComponentLoaderArgs<MyComponentData>) => {
+  // Getting `weaverse` client instance and component's `data` from component's loader function's arguments
   let {weaverse, data} = args;
-  let {storefront, request} = weaverse;
-  let selectedOptions = getSelectedProductOptions(request);
+  let {storefront, request, env} = weaverse;
 
   if (data.product) {
     return await storefront.query<ProductInfoQuery>(PRODUCT_QUERY, {
       variables: {
+        // `product.handle` should be a `string` since `MyComponentData` type is passed to `ComponentLoaderArgs<T>`
         handle: data.product.handle,
-        selectedOptions,
+        // Using `request` or `env` if needed, they are available as `weaverse` instance's properties
+        selectedOptions: getSelectedProductOptions(request);,
         language: storefront.i18n.language,
         country: storefront.i18n.country,
       },
