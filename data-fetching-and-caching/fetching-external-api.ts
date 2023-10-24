36cec9f36239aeff191bd5c2e5d4acb50bed069a
Tuan Anh (Leo) Huynh <huynhtuananh21895@gmail.com>
2023-10-24 17:40:52 +0700

    Update fetching-external-api.ts

--- data-fetching-and-caching/fetching-external-api.ts
+++ data-fetching-and-caching/fetching-external-api.ts
@@ -12,11 +12,10 @@ export let loader = async ({weaverse}: ComponentLoaderArgs<HeroSectionData>) => 
   let API = `https://pokeapi.co/api/v2/pokemon/pikachu`
 
   // The component's `props.loaderData` type will be `PokemonData`
-  let pokemon = await fetchWithCache<PokemonData>(API, {
+  return await fetchWithCache<PokemonData>(API, {
     headers: {
       'Content-Type': 'application/json',
       'X-API-KEY': env.POKEMON_API_KEY,
     },
   });
-  return pokemon;
 };
