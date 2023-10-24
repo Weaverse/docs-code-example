8cd8c31f2615f7ea0ab3c33f4e87b1edd710470f
Tuan Anh (Leo) Huynh <huynhtuananh21895@gmail.com>
2023-10-24 17:32:51 +0700

    Update load-page.ts

--- rendering-a-page/load-page.ts
+++ rendering-a-page/load-page.ts
@@ -3,12 +3,12 @@
 import { json } from '@shopify/remix-oxygen';
 import { type RouteLoaderArgs } from '@weaverse/hydrogen';
 
-export async function loader(args: RouteLoaderArgs) {
-  let {context} = args;
+export async function loader({context}: RouteLoaderArgs) {
+  let {weaverse} = context;
 
   return json({
     // The key prop for a Weaverse page must always be `weaverseData`
-    weaverseData: await context.weaverse.loadPage(),
+    weaverseData: await weaverse.loadPage(),
     // Additional page data...
   });
 }
