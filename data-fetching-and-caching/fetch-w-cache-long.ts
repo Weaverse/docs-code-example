d84af812da2570e56bbcd353e3388f00d716855f
Tuan Anh (Leo) Huynh <huynhtuananh21895@gmail.com>
2023-10-24 17:40:24 +0700

    Create fetch-w-cache-long.ts

---
+++ data-fetching-and-caching/fetch-w-cache-long.ts
@@ -0,0 +1,12 @@
+import type {ComponentLoaderArgs} from '@weaverse/hydrogen';
+
+export let loader = async ({weaverse}: ComponentLoaderArgs<HeroSectionData>) => {
+  let {fetchWithCache, storefront} = weaverse;
+  let API = `https://external-api`
+
+  // The component's `props.loaderData` type will be `PokemonData`
+  return await fetchWithCache(API, {
+    strategy: storefront.CacheLong(),
+    // request init options...
+  });
+};
