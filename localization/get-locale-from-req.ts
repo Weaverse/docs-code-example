d69ae12abd449481b5c43622e56fb90827939f5b
Tuan Anh (Leo) Huynh <huynhtuananh21895@gmail.com>
2023-10-28 15:51:38 +0700

    Create get-locale-from-req.ts

---
+++ localization/get-locale-from-req.ts
@@ -0,0 +1,19 @@
+// <root>/app/lib/utils.ts
+
+import {countries} from '~/data/countries';
+
+export function getLocaleFromRequest(request: Request): I18nLocale {
+  const url = new URL(request.url);
+  const firstPathPart =
+    '/' + url.pathname.substring(1).split('/')[0].toLowerCase();
+
+  return countries[firstPathPart]
+    ? {
+        ...countries[firstPathPart],
+        pathPrefix: firstPathPart,
+      }
+    : {
+        ...countries['default'],
+        pathPrefix: '',
+      };
+}
