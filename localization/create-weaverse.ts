286e9cafb95237404cc72e93e03cbd741be2db5a
Tuan Anh (Leo) Huynh <huynhtuananh21895@gmail.com>
2023-10-28 15:55:25 +0700

    Create create-weaverse.ts

---
+++ localization/create-weaverse.ts
@@ -0,0 +1,22 @@
+// <root>/app/weaverse/create-weaverse.server.ts
+
+import {Storefront} from '@shopify/hydrogen';
+import {I18nLocale, WeaverseClient} from '@weaverse/hydrogen';
+import {countries} from '~/data/countries';
+
+type CreateWeaverseArgs = {
+  storefront: Storefront<I18nLocale>;
+  request: Request;
+  env: Env;
+  cache: Cache;
+  waitUntil: ExecutionContext['waitUntil'];
+};
+
+export function createWeaverseClient(args: CreateWeaverseArgs) {
+  return new WeaverseClient({
+    ...args,
+    countries,
+    themeSchema,
+    components,
+  });
+}
